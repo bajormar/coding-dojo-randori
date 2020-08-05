34d4d54c131b9fdb3c28517480e8325a232563ab
bajormar <marius.bajorunas@gmail.com>
2020-08-05 16:06:20 +0300

    Solution

--- main.ts
+++ main.ts
@@ -1,3 +1,45 @@
-export const calculate = () => {
-    return null;
+const SymbolMap = {
+    'M': 1000,
+    'D': 500,
+    'C': 100,
+    'L': 50,
+    'X': 10,
+    'V': 5,
+    'I': 1,
+} as const;
+
+const times = (count: number) => Array.from(Array(count).keys());
+
+export const toRoman = (num: number) => {
+    const numberArray = num.toString().split('').map(Number).map((digit, index) => {
+        return digit * 10 ** (num.toString().length - index - 1)
+    }).filter(Boolean)
+
+    const result = [];
+
+    numberArray.forEach(digit => {
+        let rem = digit;
+        for (const symbol in SymbolMap) {
+            const value = SymbolMap[symbol];
+            const subtractValue = (10 ** (digit.toString().length - 1));
+
+            if(value - digit === subtractValue) {
+                //exception
+                result.push(Object.keys(SymbolMap).find(key => SymbolMap[key] === subtractValue));
+                result.push(symbol);
+                return;
+            }
+
+            const repeat = Math.floor(rem / value);
+            if(repeat > 0) {
+                times(repeat).forEach(() => {
+                    result.push(symbol);
+                })
+                rem = rem % value;
+            }
+        }
+    })
+
+
+    return result.join('');
 }
--- test.ts
+++ test.ts
@@ -1,7 +1,18 @@
-import {calculate} from './main';
+import {toRoman} from './main';
 
 describe('Calculate', () => {
     it('should return null', () => {
-        expect(calculate()).toBeNull();
+        expect(toRoman(1)).toBe('I');
+        expect(toRoman(2)).toBe('II');
+        expect(toRoman(3)).toBe('III');
+        expect(toRoman(4)).toBe('IV');
+        expect(toRoman(5)).toBe('V');
+        expect(toRoman(6)).toBe('VI');
+        expect(toRoman(7)).toBe('VII');
+        expect(toRoman(8)).toBe('VIII');
+        expect(toRoman(9)).toBe('IX');
+        expect(toRoman(10)).toBe('X');
+        expect(toRoman(585)).toBe('DLXXXV');
+        expect(toRoman(999)).toBe('CMXCIX');
     })
 })
