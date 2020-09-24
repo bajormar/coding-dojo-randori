17d09aa8ba7a5fc8ac15092d557be8caf4f6b4cb
bajormar <marius.bajorunas@gmail.com>
2020-09-24 14:59:33 +0300

    Add PR template

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
-export const calculate = () => {
-    return null;
-}
+var Pusher = require('pusher');
+
+var pusher = new Pusher({
+    appId: '1057826',
+    key: '3ea82fd06881041915ed',
+    secret: 'bf780a496b51d78d4959',
+    cluster: 'eu',
+    encrypted: true
+});
+
+pusher.trigger('my-channel', 'my-event', {
+    'message': 'hello world'
+});
